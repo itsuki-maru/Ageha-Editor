785b60c8af5439e5c3a3e5203baf736e9a3c76fd
itsuki-maru <marumaru.developer@gmail.com>
2025-08-10 11:37:57 +0900

    Update main.rs. Fix log control.

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -13,16 +13,10 @@ mod config;
 mod schema;
 
 fn main() {
-    // ログ設定
-    tracing_subscriber::registry()
-        .with(
-            tracing_subscriber::EnvFilter::try_from_default_env()
-                .unwrap_or_else(|_e| "Middleware Debug".into()),
-        )
-        .with(tracing_subscriber::fmt::layer())
-        .init();
+    // 初期化処理（引数の取得と環境変数の設定）
+    unsafe { env::set_var("RUST_LOG", "ageha=error".to_string()); } // info, debug, error 開発時は適宜変更
 
-    // 初期化処理
+    // 引数の処理
     let args: Vec<String> = env::args().collect();
     if args.len() > 1 {
         let file_path = &args[1];
@@ -34,6 +28,16 @@ fn main() {
             env::set_var("LAUNCH_ARGS_FILE_PATH", "".to_string());
         }
     }
+
+    // ログ設定
+    tracing_subscriber::registry()
+        .with(
+            tracing_subscriber::EnvFilter::try_from_default_env()
+                .unwrap_or_else(|_e| "ageha=error".into()),
+        )
+        .with(tracing_subscriber::fmt::layer())
+        .init();
+
     info!("Launch args file: {}", &CONFIG.args_file_path);
     ageha_lib::run()
 }
